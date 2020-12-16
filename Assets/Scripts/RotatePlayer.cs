40e8d93b7ed2ae2cee89c8a1774094b2c1cbc4ed
Joel <fallendemongod@gmail.com>
2020-12-16 18:44:51 -0500

    Movement

---
+++ Assets/Scripts/RotatePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate()
+    {
+        // Generate a plane that intersects the transform's position with an upwards normal.
+        var playerPlane = new Plane(Vector3.up, transform.position);
+
+        // Generate a ray from the cursor position
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        var hitdist = 0.0f;
+        // If the ray is parallel to the plane, Raycast will return false.
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            // Get the point along the ray that hits the calculated distance.
+            var targetPoint = ray.GetPoint(hitdist);
+
+            // Determine the target rotation.  This is the rotation if the transform looks at the target point.
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            // Smoothly rotate towards the target point.
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+        }
+    }
+}
