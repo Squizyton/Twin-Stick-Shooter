ebca49f6ceea4ebfb25a653df02f303d77c90d59
Joel <fallendemongod@gmail.com>
2020-12-16 19:08:22 -0500

    Interactable

---
+++ Assets/Scripts/Interactable/Crate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        
+    }
+}
